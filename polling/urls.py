39c8f9650eecb830616b27f0adb93c15d99fc51e
Luis Conejo-Alpizar <luisconej@gmail.com>
2022-12-13 18:18:02 -0800

    Adding latest changes

---
+++ polling/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from polling.views import PollDetailView, PollListView
+
+urlpatterns = [
+    path('', PollListView.as_view(), name="poll_index"),
+    path('polls/<int:pk>', PollDetailView.as_view(), name="poll_detail")
+]
